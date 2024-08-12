db58a77d8ca3e78b76318edb7a14d316c563dc51
haurel <hilaurell@gmail.com>
2024-08-12 21:04:32 +0300

    WIP - Import SpringConfiguration from common module

--- apis/backend/src/main/java/com/tms/api/ApiApplication.java
+++ apis/backend/src/main/java/com/tms/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.tms.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.configuration.SpringConfiguration;
 
 @SpringBootApplication
+@Import(SpringConfiguration.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
